e1223e0d8f72f207f51c9de64b74174b27e86a0f
davidsonvitorino <davidsonalves47@gmail.com>
2022-07-12 20:57:41 -0300

    Atualizado

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const boxes = document.querySelectorAll(".box");
 const checkBoxes = () => {
     const triggerBottom = (window.innerHeight / 5) * 4;
     boxes.forEach((box) => {
-        constboxTop = box.getBoundingClientRect().top;
+        const boxTop = box.getBoundingClientRect().top;
         if (boxTop < triggerBottom) box.classList.add("show");
         else box.classList.remove("show");
     });
